b2e4562acc804addc94e19b93348d2eb2e88ba4d
Sergey Khokhlov <sergey.a.khokhlov@gmail.com>
2017-03-25 11:19:05 +0300

    Update DB interaction and add new routes

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,67 +4,77 @@ import java.sql.DriverManager;
 import java.sql.Statement;
 import java.sql.ResultSet;
 
+import com.google.gson.Gson;
+
 import static spark.Spark.*;
 
 public class Main {
 
-    private static void initialiseDB() {
+    private static Connection db() {
         Connection c = null;
-        Statement stmt = null;
         try {
             Class.forName("org.postgresql.Driver");
             c = DriverManager
                     .getConnection("jdbc:postgresql://localhost:5432/docker",
                             "docker", "docker");
+        } catch (Exception e) {
+            System.err.println(e.getClass().getName() + ": " + e.getMessage());
+        }
 
+        return c;
+    }
+
+    private static int initialiseDB() {
+        Connection c = db();
+        Statement stmt;
+        try {
             stmt = c.createStatement();
             String sql = "CREATE TABLE LIST " +
-                    "(ID INT PRIMARY KEY     NOT NULL," +
+                    "(ID SERIAL PRIMARY KEY     NOT NULL," +
                     " NAME           TEXT    NOT NULL, " +
                     " NUMBER            INT     NOT NULL )";
             stmt.executeUpdate(sql);
             stmt.close();
+
             c.close();
+
+            return 0;
         } catch (Exception e) {
             System.err.println(e.getClass().getName() + ": " + e.getMessage());
-//            System.exit(0);
+
+            return 1;
         }
     }
 
-    private static void insertItem(ListItem data) {
-        Connection c = null;
-        Statement stmt = null;
+    private static int insertItem(ListItem data) {
+        Connection c = db();
+        Statement stmt;
         try {
-            Class.forName("org.postgresql.Driver");
-            c = DriverManager
-                    .getConnection("jdbc:postgresql://localhost:5432/docker",
-                            "docker", "docker");
             c.setAutoCommit(false);
 
             stmt = c.createStatement();
-            String sql = "INSERT INTO LIST (ID,NAME,NUMBER) "
-                    + "VALUES (" + data.Id + ", '" + data.Name + "', " + data.Number + " );";
+            String sql = "INSERT INTO LIST (NAME,NUMBER) "
+                    + "VALUES ('" + data.Name + "', " + data.Number + " );";
             stmt.executeUpdate(sql);
 
             stmt.close();
             c.commit();
             c.close();
+
+            return 0;
         } catch (Exception e) {
             System.err.println(e.getClass().getName() + ": " + e.getMessage());
-            System.exit(0);
+
+            return 1;
         }
     }
 
     private static ArrayList<ListItem> getItems() {
-        Connection c = null;
-        Statement stmt = null;
+        Connection c = db();
+        Statement stmt;
         ArrayList<ListItem> result = new ArrayList<>();
 
         try {
-            Class.forName("org.postgresql.Driver");
-            c = DriverManager
-                    .getConnection("jdbc:postgresql://localhost:5432/docker",
-                            "docker", "docker");
             c.setAutoCommit(false);
 
             stmt = c.createStatement();
@@ -80,18 +90,19 @@ public class Main {
             rs.close();
             stmt.close();
             c.close();
+
         } catch (Exception e) {
             System.err.println(e.getClass().getName() + ": " + e.getMessage());
-            System.exit(0);
+
         }
 
         return result;
     }
 
     private static class ListItem {
-        public int Id;
-        public String Name;
-        public int Number;
+        int Id;
+        String Name;
+        int Number;
 
         ListItem(int inputId, String inputName, int inputNumber) {
             Id = inputId;
@@ -105,15 +116,65 @@ public class Main {
         }
     }
 
+    private static class HttpStatus {
+        int Status;
+        String Message;
+
+        HttpStatus(int inputStatus, String inputMessage) {
+            Status = inputStatus;
+            Message = inputMessage;
+        }
+
+        HttpStatus(int inputStatus) {
+            Status = inputStatus;
+        }
+    }
+
     // docker run --rm -p 5432:5432 --name pg_test eg_postgresql
     public static void main(String[] args) {
         initialiseDB();
-        ListItem a = new ListItem("ASD", 123);
-        insertItem(a);
-        ArrayList<ListItem> b = getItems();
-        System.out.println(b.get(0).Name);
+
+        after((request, response) -> response.header("Content-Encoding", "gzip"));
+
+        notFound((req, res) -> {
+            res.type("application/json");
+            Gson gson = new Gson();
+            HttpStatus response = new HttpStatus(404, "Not Found");
+            return gson.toJson(response);
+        });
+
+        internalServerError((req, res) -> {
+            res.type("application/json");
+            Gson gson = new Gson();
+            HttpStatus response = new HttpStatus(500, "Internal server error");
+            return gson.toJson(response);
+        });
 
         get("/status", (req, res) -> "Works");
+
+        get("/items/everything", (req, res) -> {
+            res.type("application/json");
+            Gson gson = new Gson();
+            ArrayList<ListItem> b = getItems();
+            return gson.toJson(b);
+        });
+
+        post("/items/:item", (req, res) -> {
+            res.type("application/json");
+            Gson gson = new Gson();
+            ListItem item = gson.fromJson(req.body(), ListItem.class);
+            HttpStatus response;
+
+            if (insertItem(item) == 0) {
+                response = new HttpStatus(200);
+            } else {
+                response = new HttpStatus(400);
+                res.status(400);
+            }
+
+            return gson.toJson(response);
+        });
+
 
     }
 
